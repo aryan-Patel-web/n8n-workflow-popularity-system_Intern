1a0768ae507eb5407c3431ddfb5cd6b33093659c
aryan-Patel-web <aryanpatel77462@gmail.com>
2025-12-16 19:35:38 +0530

    internship proj  = working on backend

--- frontend/src/Landingpage.jsx
+++ frontend/src/Landingpage.jsx
@@ -8,6 +8,8 @@ const API_BASE_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:8000'
   : 'https://speakgenie-n8n-backend.onrender.com';
 
+
+  
 const LandingPage = () => {
   // State Management
   const [workflows, setWorkflows] = useState([]);
